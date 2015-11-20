a526a502f09db3c92252145165220d5321976125
Dylan <dwfoster1@catamount.wcu.edu>
2015-11-20 13:29:00 -0500

    Fixed Proximity completely

--- app/src/main/java/com/example/dylan/manhunt/ProximityReceiver.java
+++ app/src/main/java/com/example/dylan/manhunt/ProximityReceiver.java
@@ -12,20 +12,20 @@ public class ProximityReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context arg0, Intent arg1) {
         // TODO Auto-generated method stub
-        // The reciever gets the Context & the Intent that fired the broadcast as arg0 & agr1
+        // The receiver gets the Context & the Intent that fired the broadcast as arg0 & agr1
 
-        String k= LocationManager.KEY_PROXIMITY_ENTERING;
+        String k = LocationManager.KEY_PROXIMITY_ENTERING;
         // Key for determining whether user is leaving or entering
 
-        boolean state=arg1.getBooleanExtra(k, false);
+        boolean state = arg1.getBooleanExtra(k, false);
         //Gives whether the user is entering or leaving in boolean form
 
         if(state){
             // Call the Notification Service or anything else that you would like to do here
-            Toast.makeText(arg0, "Welcome to my Area", Toast.LENGTH_SHORT).show();
+            Toast.makeText(arg0, "You have entered tag zone", Toast.LENGTH_SHORT).show();
         }else{
             //Other custom Notification
-            Toast.makeText(arg0, "Thank you for visiting my Area,come back again !!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(arg0, "You have left tag zone", Toast.LENGTH_SHORT).show();
 
         }
 
